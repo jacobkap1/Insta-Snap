be5d08ab875f78f78ac25e7b90b213c32a9a4584
Sai Krupa Reddy Surarapu <saikrupar82@gmail.com>
2024-06-29 05:52:00 -0500

    New Commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,44 +1,52 @@
-// server.js
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const pool = require('./db'); // Import the database connection
+import express, { json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import authRouter from './routes/auth-routes.js';
+import usersRouter from './routes/users-routes.js';
+import models from './models/index.js'; // Import Sequelize models
+
+dotenv.config(); // Load environment variables from .env file
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 5000;
 
-app.use(cors());
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 5000;
 
-// Define the /api/data endpoint
-app.get('/api/data', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
-});
+const corsOptions = { credentials: true, origin: process.env.URL || '*' };
+
+app.use(cors(corsOptions));
+app.use(json()); // Ensure JSON parsing middleware is applied
+app.use(cookieParser());
+
+app.use('/', express.static(join(__dirname, 'public')));
+app.use('/api/users', usersRouter);
+app.use('/api/auth', authRouter); // Corrected the path and method
 
 // Endpoint to test database connection
 app.get('/users', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM users');
-    res.json(result.rows);
+    const users = await models.User.findAll();
+    res.json(users);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
 });
 
-process.on('SIGINT', () => {
-  pool.end()
-    .then(() => {
-      console.log('Pool has been closed successfully');
-      process.exit(0);
-    })
-    .catch((err) => {
-      console.error('Error closing pool', err.stack);
-      process.exit(1);
-    });
+process.on('SIGINT', async () => {
+  try {
+    await models.sequelize.close();
+    console.log('Sequelize has been closed successfully');
+    process.exit(0);
+  } catch (err) {
+    console.error('Error closing Sequelize', err.stack);
+    process.exit(1);
+  }
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
